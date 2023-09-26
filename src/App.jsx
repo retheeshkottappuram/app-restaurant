1b547f0069b803926b92421aa2c3824946f0d4cc
DESKTOP-HFIRIKR\CYBER CELL <retheeshkottappuram@gmail.com>
2023-09-26 22:18:26 +0530

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Header from './components/Header/Header'
-
+import Welcome from './pages/Welcome/Welcome'
+import Footer from './components/Footer/Footer'
+import Partner from './components/Partner/Partner'
+import Booking from './pages/Booking/Booking'
+import Dishes from './pages/Dishes/Dishes'
 
 function App() {
   return (
     <>
 <Header />
+<Welcome />
+<Partner />
+<Booking />
+<Dishes />
+<Footer />
     </>
     
   )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,102 +1,182 @@
-import {styled} from '@mui/system'
-import { Box, Typography } from '@mui/material'
-import React from 'react'
-import CustomButton from '../CustomButton/CustomButton';
+import { styled } from "@mui/system";
+import { Box, Typography } from "@mui/material";
+import React, { useState } from "react";
+import CustomButton from "../CustomButton/CustomButton";
+import logoImage from "../../assets/logo.png";
 
-const nav_titles = [
-    {
-      path:'/',
-      display:'Home'
-    },
-    {
-      path:'/',
-      display:'Dishes'
-    },
-    {
-      path:'/',
-      display:'Services'
-    },
-    {
-      path:'/',
-      display:'About Us'
+import MenuIcon from "@mui/icons-material/Menu";
+import FeaturedPlayListIcon from "@mui/icons-material/FeaturedPlayList";
+import MiscellaneousServicesIcon from "@mui/icons-material/MiscellaneousServices";
+import HomeIcon from "@mui/icons-material/Home";
+import ContactsIcon from "@mui/icons-material/Contacts";
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+
+function Header() {
+  const [mobileMenu, setMobileMenu] = useState({ left: false });
+
+  const list = (anchor) => (
+    <Box
+      sx={{
+        width: anchor === "top" || anchor === "bottom" ? "auto" : 250,
+      }}
+      role="presentation"
+      onClick={toogleDrawer(anchor, false)}
+      onKeyDown={toogleDrawer(anchor, false)}
+    >
+      <List>
+        {nav_titles.map((item, index) => (
+          <ListItem key={item.index} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index === 0 && <HomeIcon />}
+                {index === 1 && <FeaturedPlayListIcon />}
+                {index === 2 && <MiscellaneousServicesIcon />}
+                {index === 3 && <ContactsIcon />}
+              </ListItemIcon>
+              <ListItemText primary={item.display} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  )
+  
+  const toogleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.type === "Tab" || event.type === "Shift")
+    ) {
+      return;
     }
-  ]
 
+
+    setMobileMenu({ ...mobileMenu, [anchor]: open });
+  };
+
+  const nav_titles = [
+    {
+      path: "/",
+      display: "Home",
+    },
+    {
+      path: "/",
+      display: "Dishes",
+    },
+    {
+      path: "/",
+      display: "Services",
+    },
+    {
+      path: "/",
+      display: "About Us",
+    },
+  ];
 
   const NavBarLinksBox = styled(Box)(({ theme }) => ({
-display: "flex",
-alignItems: "center",
-justifyContent: "center",
-gap: theme.spacing(3),
-[theme.breakpoints.down("md")]: {
-    display:"none",
-},
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: theme.spacing(3),
+    [theme.breakpoints.down("md")]: {
+      display: "none",
+    },
   }));
 
   const NavBarLink = styled(Typography)(() => ({
     fontSize: "15px",
-    color:'#4F5361',
-    fontWeight: 'bold',
-    cursor: 'pointer',
-    '&:hover':{
-        color:'#fff'
-    }
+    color: "#4F5361",
+    fontWeight: "bold",
+    cursor: "pointer",
+    "&:hover": {
+      color: "#fff",
+    },
+  }));
 
-      }));
+  const NavBarLogo = styled("img")(({ theme }) => ({
+    cursor: "pointer",
+    [theme.breakpoints.down("md")]: {
+      display: "none",
+    },
+  }));
 
+  const CustomMenuIcon = styled(MenuIcon)(({ theme }) => ({
+    cursor: "pointer",
+    display: "none",
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.down("md")]: {
+      display: "block",
+    },
+  }));
 
-function Header() {
   return (
-<Box
-    sx={{
-        display:'flex',
-        alignItems:'center',
-        justifyContent:'space-between',
-        padding:'40px',
-        maxWidth:'auto',
-        backgroundColor:'#FED801',
-        marginLeft:'0px',
-        marginBottom:'-24px'
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "40px",
+        maxWidth: "auto",
+        backgroundColor: "#FED801",
+        marginLeft: "0px",
+        marginBottom: "-24px",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "2.5rem",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <CustomMenuIcon onClick={toogleDrawer("left", true)} />
 
-    }}
->
+          <Drawer
+            anchor="left"
+            open={mobileMenu["left"]}
+            onClose={toogleDrawer("left", false)}
+          >
+            {list("left")}
+          </Drawer>
+          <NavBarLogo src={logoImage} />
+        </Box>
 
-
-<Box
-    sx={{
-        display:'flex',
-        alignItems:'center',
-        justifyContent:'center',
-        gap:'2.5rem'
-    }}>
-<NavBarLinksBox>
-    {nav_titles.map((item,index)=>(
-    <NavBarLink variant="body2">
-    {item.display}
-</NavBarLink>
-    ))
-    }
-</NavBarLinksBox>
-    
-
+        <NavBarLinksBox>
+          {nav_titles.map((item, index) => (
+            <NavBarLink variant="body2">{item.display}</NavBarLink>
+          ))}
+        </NavBarLinksBox>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "1rem",
+        }}
+      >
+        <NavBarLink variant="body2">Sign Up</NavBarLink>
+        <CustomButton
+          backgroundColor="#0F1B4C"
+          color="#fff"
+          buttonText="Register"
+        />
+      </Box>
     </Box>
-    <Box sx={{
-        display:'flex',
-        alignItems:'center',
-        justifyContent:'center',
-        gap:'1rem'
-    }}>
-    <NavBarLink variant="body2">
-    Sign Up
-</NavBarLink>
-<CustomButton 
-backgroundColor='#0F1B4C'
-color='#fff'
-buttonText="Register"
-/>
-    </Box>
-</Box>
-  )
+  );
 }
 
-export default Header
+export default Header;
